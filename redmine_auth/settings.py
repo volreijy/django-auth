9294c6746596e98200ce586aaa30e9e17c6f8944
volreijy <nakano.masatoshi@stf.nifty.co.jp>
2017-03-22 18:31:11 +0900

    initial

---
+++ redmine_auth/settings.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+#REDMINE_SERVER_URL = getattr(settings, 'REDMINE_SERVER_URL', 'http://localhost')
+REDMINE_SERVER_URL = getattr(settings, 'http://localhost:3000', 'http://localhost')
